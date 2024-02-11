09542070daa27a64d26af8b90da0ca9b65b478e3
Raushan <roushankumarsingh2018@gmail.com>
2024-02-11 12:31:13 +0530

    added some stying

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail 
+      image='https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg'
+      title='First Meetup' 
+      address='Some Street 4, Some City'
+      description='The Meetup description'
+    />
+  ) 
+}
