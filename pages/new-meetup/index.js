d7e5ae4188c595c34bad4485a381ef505359428e
Raushan <roushankumarsingh2018@gmail.com>
2024-02-11 12:05:46 +0530

    added /id

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+//our-domain.com/new-meetup
+
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
